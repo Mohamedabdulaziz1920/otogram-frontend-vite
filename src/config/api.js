a90899e875c579985143c610754a40b49bac03f3
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-10-10 21:46:09 +0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://otogram.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://otogram-backend-production-d98d.up.railway.app';
 
 export default API_URL;
